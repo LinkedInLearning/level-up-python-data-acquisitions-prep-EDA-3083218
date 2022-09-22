8dd23ba982a9c2c8bb1c529f3d36fa090918af7a
Seth Berry <seth.berry@nd.edu>
2022-09-22 11:35:48 -0400

    Removed folder name from read.

--- code/chapter_03_video_01.py
+++ code/chapter_03_video_01.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 pd.set_option('display.max_columns', None)
 
-employees = pd.read_csv('data/level_up_data.csv')
+employees = pd.read_csv('level_up_data.csv')
 
 employees.describe(include='all')
