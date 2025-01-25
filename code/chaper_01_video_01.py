e8cdc3c11974b9b3de1164455b12f376e781e656
Anta <74151813+antaosina@users.noreply.github.com>
2025-01-25 13:26:04 +0000

    Pending changes exported from your codespace

--- code/chaper_01_video_01.py
+++ code/chaper_01_video_01.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 link = "https://3083218.youcanlearnit.net/geographytables.html"
 
-area_data = pd.read_html(link)
+area_data = pd.read_html(link, match="Area")
 
 area_data = area_data.pop()
+area_data
